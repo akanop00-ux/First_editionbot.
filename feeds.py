16e92a6537fd0fb533b3d4dd70042db1d2f02044
akanop00-ux <akanop00@gmail.com>
2026-09-11 09:32:21 -0700

    Create feeds.py

---
+++ feeds.py
@@ -0,0 +1,13 @@
+# Edit this list to control what news the bot pulls
+FEEDS = {
+    "tech": [
+        "https://feeds.arstechnica.com/arstechnica/index",
+        "https://www.theverge.com/rss/index.xml",
+    ],
+    "world": [
+        "https://feeds.bbci.co.uk/news/world/rss.xml",
+    ],
+    "opportunities": [
+        # Add RSS feeds for grants, jobs, scholarships, etc.
+    ],
+}
